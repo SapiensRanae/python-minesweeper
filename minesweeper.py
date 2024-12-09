aa4fcff6dd30798d7e90e73c93f810dd560d32d6
Artem Mitrokhin <105779435+SapiensRanae@users.noreply.github.com>
2024-12-09 17:45:02 +0100

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -30,7 +30,7 @@ class minesweeper_class():
     window = tk.Tk()
     window.title('Сапер')
     
-    window.iconphoto(False, tk.PhotoImage(file='icon.ico'))
+    window.iconphoto(False, tk.PhotoImage(file='icon.png'))
 
     rows = 10
     colums = 10
